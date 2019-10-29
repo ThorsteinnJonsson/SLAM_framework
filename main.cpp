ba3a842a614f10505ef3e397b194003cf72457cd
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2019-10-29 22:24:32 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<iomanip>
+#include<chrono>
+
+#include<opencv2/core/core.hpp>
+
+int main(){
+  std::cout << "Hello World!\n";
+}
