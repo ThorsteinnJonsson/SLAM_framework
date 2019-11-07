f1bcda8a6cbcda40a810c54c38858217f4f48745
ThorsteinnJonsson <thorsteinnbaldvin@gmail.com>
2019-11-08 00:06:17 +0900

    WIP: Implement classes for graph-based SLAM

---
+++ src/keyframe.cpp
@@ -0,0 +1,5 @@
+#include "keyframe.h"
+
+KeyFrame::KeyFrame(Frame &F, Map* pMap, KeyFrameDatabase* pKFDB) {
+  // TODO
+}
---
+++ src/keyframe_database.h
@@ -0,0 +1,13 @@
+#ifndef SRC_KEYFRAME_DATABASE_H_
+#define SRC_KEYFRAME_DATABASE_H_
+
+class KeyframeDatabase {
+public:
+  KeyframeDatabase();
+  ~KeyframeDatabase();
+
+private:
+
+};
+
+#endif // SRC_KEYFRAME_DATABASE_H_
